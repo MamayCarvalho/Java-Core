4846e77342ff489aee20ea9e06d3dd6b0de526a5
MamayCarvalho <115594676+MamayCarvalho@users.noreply.github.com>
2023-08-19 04:05:16 -0300

    ControleFinanceiro.java
    Update

--- Java-Core/src/entities/ControleFinanceiro.java
+++ Java-Core/src/entities/ControleFinanceiro.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 public class ControleFinanceiro {
 	
-	private double salario;
 	private double valorDespesa;
 	private double saldo;
 	private String despesa;
@@ -16,10 +15,9 @@ public class ControleFinanceiro {
 		
 	}
 	
-	public ControleFinanceiro(double salario, double valorDespesa, String despesa, double saldo) {
-		this.salario = salario;
+	public ControleFinanceiro(double valorDespesa, String despesa, double saldo) {
 		this.valorDespesa = valorDespesa;
-		this.despesa = despesa;
+		this.despesa = despesa; 
 		this.saldo = saldo;
 	}
 	
@@ -37,10 +35,6 @@ public class ControleFinanceiro {
 		return saldo;
 	}
 
-	public String getDespesa() {
-		return despesa;
-	}
-
 	public void setDespesa(String despesa) {
 		this.despesa = despesa;
 	}
@@ -51,38 +45,48 @@ public class ControleFinanceiro {
 		
 	}
 	public void addDespesa(double valorDespesa) {
-		this.valorDespesa -= (valorDespesa + getSaldo());
+		this.valorDespesa += valorDespesa;
+		this.saldo -= valorDespesa;
 	}
 	
 	public void selecaoMenu (Scanner sc){
-		System.out.println("Selecione a opção desejada: \n1 - Cadastro de Saldo \n2 - Cadastro de Despesa \n3 - Visualizar Saldo");
+		System.out.println("Selecione a opção desejada: \n1 - Cadastro de Saldo \n2 - Cadastro de Despesa \n3 - Visualizar Saldo \n4 - Encerrar.");
 		int selecao = sc.nextInt();
 		switch (selecao) {
 		case 1:
 			System.out.println("Você escolheu Cadastro de Saldo!!");
-			System.out.println("Informe o valor de saldo que deseja cadastrar: ");
+			System.out.println("\nInforme o valor de saldo que deseja cadastrar: \n");
 			double saldo = sc.nextDouble();
 			addSaldo(saldo);
 			dadosConta();
+			selecaoMenu(sc);
+			
 			break;
 		case 2: 
 			System.out.println("Você escolheu Cadastro de Despesa ");
 			System.out.println("Informe o nome do Produto que você deseja gastar o dinheiro: ");
 			String despesa = sc.next();
 			setDespesa(despesa);
+			
 			System.out.println("Informe o valor do Produto: ");
 			double valorDespesa = sc.nextDouble();
+			setValorDespesa(valorDespesa);
 			addDespesa(valorDespesa);
 			dadosConta();
+			outraCompra(sc);
 			
 			break;
 		case 3: 
-			System.out.println("Você escolheu Visualização de Saldo");
 			dadosConta();
-			
+			selecaoMenu(sc);
+		
+			break;
+		case 4: 
+			System.out.println("Obrigada por utilizar o nosso sistema. Nos vemos na próxima!! ");
 			break;
 		default: 
 			System.out.println("Opção inválida! Selecione uma opção válida.");
+			selecaoMenu(sc);
 			break;
 			}
 		}
@@ -94,5 +98,32 @@ public class ControleFinanceiro {
 	public String toString() {
 		return "O valor do seu saldo é: R$" + String.format("%.2f", getSaldo());
 	}
+	public void outraCompra(Scanner sc) {
+		System.out.println("Deseja cadastrar uma nova Despesa? (S/N)");
+		char letra = sc.next().charAt(0);
+		switch(letra) {
+		case 's','S': 
+			
+			System.out.println("Informe o nome do Produto que você deseja gastar o dinheiro: ");
+			String despesa = sc.next();
+			setDespesa(despesa);
+			System.out.println("Informe o valor do Produto: ");
+			double valorDespesa = sc.nextDouble();
+			addDespesa(valorDespesa);
+			dadosConta();
+			outraCompra(sc);
+			break;
+			
+		case 'n','N':
+			
+			selecaoMenu(sc);
+		
+		default: 
+			System.out.println("Opção inválida! Selecione uma opção válida.");
+			selecaoMenu(sc);
+			break;
+		}
+	}
+	
 }
 
