06c5c98e72d3cdea8f27d7f536b92781dd565dbc
MamayCarvalho <115594676+MamayCarvalho@users.noreply.github.com>
2023-08-19 04:03:49 -0300

    Start_ControleFinanceiro.java
    Ajuste

--- Java-Core/src/aplication/Start_ControleFinanceiro.java
+++ Java-Core/src/aplication/Start_ControleFinanceiro.java
@@ -11,15 +11,13 @@ public class Start_ControleFinanceiro {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		ControleFinanceiro programa = new ControleFinanceiro(0, 0, null, 0);
+		ControleFinanceiro programa = new ControleFinanceiro(0, null, 0);
+		
+		System.out.println("Informe o valor do seu sálario para iniciar o Programa: ");
+		double saldo = sc.nextDouble();
+		programa.addSaldo(saldo);
 		
 		programa.selecaoMenu(sc);
-		
-		
-		
-		
-		
-		
 		
 		
 		
