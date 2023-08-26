4edc964d9c19a4deb1bce051d8a3539e1d6d1d25
MamayCarvalho <115594676+MamayCarvalho@users.noreply.github.com>
2023-08-25 21:57:50 -0300

    aula 25/08

--- src/revisao/Agenda.java
+++ src/revisao/Agenda.java
@@ -41,7 +41,7 @@ public class Agenda {
 	public String getEndereco() {
 		return endereco;
 	}
-	public void setEndereco(String endereco) {
+	private void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
 
